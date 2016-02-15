7303f64488cc988e56e726685900653f7db49b88
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:09:33 +0100

    Create fichierEntree.h

---
+++ fichierEntree.h
@@ -0,0 +1,18 @@
+void fichierEntree(char* fileName, Data* data);
+
+#ifndef FICHIERENTREE_H
+#define	FICHIERENTREE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* FICHIERENTREE_H */
+
