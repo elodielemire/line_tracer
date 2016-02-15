b8cddcd8b0ecd1d8760fd508bd058b927a154275
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:10:15 +0100

    Create droitesVertes.h

---
+++ droitesVertes.h
@@ -0,0 +1,18 @@
+void droitesVertes(Image* image, Point* point, Data* data);
+
+#ifndef DROITESVERTES_H
+#define	DROITESVERTES_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DROITESVERTES_H */
+
