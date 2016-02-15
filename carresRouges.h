64aa94deb542529077e2a34ecda697ea37f58a57
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:12:54 +0100

    Create carresRouges.h

---
+++ carresRouges.h
@@ -0,0 +1,18 @@
+void carresRouges(Image* image, Point* point, Data* data);
+
+#ifndef CARRESROUGES_H
+#define	CARRESROUGES_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CARRESROUGES_H */
+
