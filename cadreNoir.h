25eacde3a796fb87bb81a88c016e463fbc6a91eb
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:14:56 +0100

    Create cadreNoir.h

---
+++ cadreNoir.h
@@ -0,0 +1,18 @@
+void cadreNoir(Image* image, Data* data) ;
+
+#ifndef CADRENOIR_H
+#define	CADRENOIR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CADRENOIR_H */
+
