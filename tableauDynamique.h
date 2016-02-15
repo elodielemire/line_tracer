086056026c012952d16c1f75351d3185ecf4688d
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:07:31 +0100

    Create tableauDynamique.h

---
+++ tableauDynamique.h
@@ -0,0 +1,18 @@
+void tableauDynamique(Image* image, Data *data);
+
+#ifndef TABLEAUDYNAMIQUE_H
+#define	TABLEAUDYNAMIQUE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TABLEAUDYNAMIQUE_H */
+
