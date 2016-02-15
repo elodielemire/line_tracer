951c417ae479e8dc4e2bce79b5fd77c0acef567c
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:01:18 +0100

    Create triBulles.h

---
+++ triBulles.h
@@ -0,0 +1,18 @@
+void triBulles(Image* image, Point* point, Data* data) ;
+
+#ifndef TRIBULLES_H
+#define	TRIBULLES_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TRIBULLES_H */
+
