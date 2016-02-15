ec37095ba727596db0f0d7d118cb63816b823d12
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:08:58 +0100

    Create fondBleu.h

---
+++ fondBleu.h
@@ -0,0 +1,18 @@
+void fondBleu(Image* image, Data* data);
+
+#ifndef FONDBLEU_H
+#define	FONDBLEU_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* FONDBLEU_H */
+
