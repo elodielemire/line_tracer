3952ab15c98fe2d1d06d457de542624bacbe9aef
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:10:51 +0100

    Create chargerImage.h

---
+++ chargerImage.h
@@ -0,0 +1,18 @@
+void chargerImage(Image* image, char* fenetre);
+
+#ifndef CHARGERIMAGE_H
+#define	CHARGERIMAGE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CHARGERIMAGE_H */
+
